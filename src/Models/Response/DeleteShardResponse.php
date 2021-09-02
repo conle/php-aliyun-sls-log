324df3a6fb9000819d43ee0fd4c7f03e8ca335e7
scissors <scissors@cencobuy.com>
2021-09-02 17:46:52 +0800

    addNameSpace

---
+++ src/Models/Response/DeleteShardResponse.php
@@ -0,0 +1,25 @@
+<?php
+namespace AliSlsLog\Models\Response;
+/**
+ * Copyright (C) Alibaba Cloud Computing
+ * All rights reserved
+ */
+
+require_once realpath(dirname(__FILE__) . '/Response.php');
+
+/**
+ * The response of the DeleteShard API from log service.
+ *
+ * @author log service dev
+ */
+class Aliyun_Log_Models_DeleteShardResponse extends Aliyun_Log_Models_Response {
+    /**
+     * Aliyun_Log_Models_DeleteShardResponse constructor
+     *
+     * @param array $header
+     *            DeleteShard HTTP response header
+     */
+    public function __construct($headers) {
+        parent::__construct ( $headers );
+    }
+}
